c18f1311f794fbf0bbbd55201aca9036a66c453c
Kolan Nikshith reddy <nkreddy100@gmail.com>
2022-04-21 21:02:30 +0530

    vector bool

---
+++ 705.design-hash-set.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=705 lang=cpp
+ *
+ * [705] Design HashSet
+ */
+
+// @lc code=start
+class MyHashSet {
+    public:
+    vector<bool> v;//(1000001,false);
+    MyHashSet(): v(1000001, false) {
+        
+    }
+    
+    void add(int key) {
+        v[key] = true;
+    }
+    
+    void remove(int key) {
+        v[key] = false;
+    }
+    
+    bool contains(int key) {
+        return v[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
+// @lc code=end
+
