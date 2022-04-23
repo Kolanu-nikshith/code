3d480887713f872543261a1b84dc473d2a069bfc
Kolan Nikshith reddy <nkreddy100@gmail.com>
2022-04-23 23:44:03 +0530

    learnt convering a string into 6 char short-string using mod and rand() % pow(6,62)

---
+++ 535.encode-and-decode-tiny-url.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=535 lang=cpp
+ *
+ * [535] Encode and Decode TinyURL
+ */
+
+// @lc code=start
+
+class Solution {
+public:
+
+    
+    string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
+    // to convert a number into 6 digit, take 6 places and mod it.
+    long long MOD = 56800235583; //6 ^62 
+    
+    unordered_map<string, string> urlMap;
+    
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        long long randomNum = rand() % MOD;
+        string tinyThing = convertToB62(randomNum);
+        while(urlMap.find(tinyThing) != urlMap.end()) {
+            randomNum = rand() % MOD;
+            tinyThing = convertToB62(randomNum);
+        }
+        urlMap[tinyThing] = longUrl;
+        return tinyThing;
+    }
+
+    //Base62 conversion.
+    string convertToB62(long long num) {
+        string ans = "";
+        while(num) {
+            int mod = num % 62;
+            ans += characters[mod];
+            num /= 62;
+        }
+        return ans;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return urlMap[shortUrl];
+    }
+ 
+
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
+// @lc code=end
+
