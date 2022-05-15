78a24777a7f8cc76ea59806c6cbdb622da27b258
Kolanu-nikshith <32254529+Kolanu-nikshith@users.noreply.github.com>
2022-05-15 18:54:44 +0530

    Time: 159 ms (25.30%), Space: 59.6 MB (94.18%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,29 +11,59 @@
  */
 class Solution {
 public:
-    int maxh=INT_MIN;
+    int maxh=0;
     int sum=0;
     int deepestLeavesSum(TreeNode* root) {
-        inorder(root, 0);
-        inorder2(root, 0);
+        // inorder(root, 0);
+        // inorder2(root, 0);
+        solver(root, 0);
         return sum;
     }
-    void inorder(TreeNode* root, int h){
-        if(root){
-            inorder(root->left, h+1);
-            if(h>maxh) maxh=h;
-            inorder(root->right, h+1);
+
+    void solver(TreeNode* root, int currh){
+        if(!root) return ;
+        //at leaf, if you see new height element, then it is the first time you have seen it. so sum = root->val.
+        //then if of same level if you see again, then add it up.
+        
+        if(!root->left and !root->right){
+            if(currh> maxh){
+                maxh = currh;
+                sum = root->val;
+            }
+            else if(currh == maxh){
+                sum+=root->val;
+            }
         }
+        solver(root->left, currh+1);
+        solver(root->right, currh+1);        
     }
     
-    void inorder2(TreeNode* root, int h){
-        if(root){
-            inorder2(root->left, h+1);
-            if(h==maxh) sum+=root->val;
-            inorder2(root->right, h+1);
-        }
-    }
+    
+    
+    
+    
+    
+    
+    
+    
+//     void inorder(TreeNode* root, int h){
+//         if(root){
+//             inorder(root->left, h+1);
+//             if(h>maxh) maxh=h;
+//             inorder(root->right, h+1);
+//         }
+//     }
+    
+//     void inorder2(TreeNode* root, int h){
+//         if(root){
+//             inorder2(root->left, h+1);
+//             if(h==maxh) sum+=root->val;
+//             inorder2(root->right, h+1);
+//         }
+//     }
 
+    
+    
     
     
 };
