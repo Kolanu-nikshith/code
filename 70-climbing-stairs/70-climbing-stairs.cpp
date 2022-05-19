8f88a14ee3a011be10ea42d104ccedf0d863ca48
Kolanu-nikshith <32254529+Kolanu-nikshith@users.noreply.github.com>
2022-05-19 15:53:40 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (38.92%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<long long int> v;
+        v.push_back(0);
+        v.push_back(1);
+        v.push_back(2);
+        
+        for(int i=3;i<=n;i++){
+            v.push_back(v[i-1]+v[i-2]);
+        }
+        return v[n];
+        
+        
+    }
+};
