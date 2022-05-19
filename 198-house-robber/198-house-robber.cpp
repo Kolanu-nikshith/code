c1414604fd9996e0100427194788e6e12c09b32d
Kolanu-nikshith <32254529+Kolanu-nikshith@users.noreply.github.com>
2022-05-19 16:22:28 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.62%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,12 +3,15 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         int dp[n];
-        if(n==1) return nums[0];
-        dp[0] =nums[0];
-        dp[1] =max(nums[1], nums[0]);
-        for(int i=2;i<n;i++){
-            dp[i] = max(nums[i]+dp[i-2], dp[i-1]);
-        }
-        return dp[n-1];
+        memset(dp, -1, sizeof(dp));
+        return recursion(nums,dp, 0);
+    
     }
+    int recursion(vector<int> & nums,int dp[], int i){
+        if(i>=nums.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        return dp[i]=max(nums[i]+ recursion(nums,dp, i+2), recursion(nums,dp, i+1));
+            
+    }
+    
 };
