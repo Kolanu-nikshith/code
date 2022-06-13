fa5d2a7c4591a86584481946b960f5a128560a49
Kolanu-nikshith <32254529+Kolanu-nikshith@users.noreply.github.com>
2022-06-13 18:09:04 +0530

    Time: 13 ms (22.10%), Space: 8.6 MB (91.19%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,18 +3,34 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
        //recursion 
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n,0));
-        //return solve(0,0,n,triangle, dp);
-        for(int i=0;i<n;i++){
-            dp[n-1][i] = triangle[n-1][i];
-        }
+//         vector<vector<int>> dp(n, vector<int>(n,0));
+//         //return solve(0,0,n,triangle, dp);
+//         for(int i=0;i<n;i++){
+//             dp[n-1][i] = triangle[n-1][i];
+//         }
         
+        // for(int i=n-2;i>=0;i--){
+        //     for(int j=i;j>=0;j--){
+        //         dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+        //     }
+        // }
+        
+        //optimise space
+            vector<int> prev(n);
+            vector<int> curr(n);
+        for(int i=0;i<n;i++){
+                    prev[i] = triangle[n-1][i];
+        }
         for(int i=n-2;i>=0;i--){
             for(int j=i;j>=0;j--){
-                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+                curr[j] = triangle[i][j] + min(prev[j], prev[j+1]);
             }
+            prev=curr;
         }
-        return dp[0][0];
+        
+        
+        
+        return prev[0];
         
     }
 
