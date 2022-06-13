614eb6651e171038f8fea1a33ee58437b2fac376
Kolanu-nikshith <32254529+Kolanu-nikshith@users.noreply.github.com>
2022-06-13 18:05:19 +0530

    Time: 4 ms (89.10%), Space: 9.1 MB (9.59%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,9 +3,23 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
        //recursion 
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        return solve(0,0,n,triangle, dp);
+        vector<vector<int>> dp(n, vector<int>(n,0));
+        //return solve(0,0,n,triangle, dp);
+        for(int i=0;i<n;i++){
+            dp[n-1][i] = triangle[n-1][i];
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
+        
     }
+
+    
+    
     int solve(int i, int j, int n, vector<vector<int>>& t, vector<vector<int>>& dp){
         if(i==n-1) return t[i][j];
         if(dp[i][j]!=-1) return dp[i][j];
