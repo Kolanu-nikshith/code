cb8ad9306e5d79aa1ce6059ea6bcfd9620c9d0d7
Kolanu Nikshith Reddy <nkreddy100@gmail.com>
2022-08-01 21:59:08 +0530

    Time: 3 ms (59.99%), Space: 5.8 MB (70.36%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
-        int i = __builtin_popcount(n);
-        return i==1?true:false;
+//         int i = __builtin_popcount(n);
+//         return i==1?true:false;
+        return ((n)&(n-1))==0? true:false;
+        
         
     }
 };
