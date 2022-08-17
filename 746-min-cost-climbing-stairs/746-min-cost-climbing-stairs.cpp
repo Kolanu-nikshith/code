c6de539892a30a23ee3ac99586aab10a7ac3185e
Kolanu Nikshith Reddy <nkreddy100@gmail.com>
2022-08-17 12:38:22 +0530

    Time: 13 ms (36.45%), Space: 13.9 MB (26.05%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int dp[cost.size()];
-        int n =cost.size();
-        // for(int i=2;i<n;i++){
-        //     dp[i] = min(dp[i-2], dp[i-1])+cost[i];
-        // }
-        memset(dp, -1, sizeof(dp));
-        return min(rec(cost, 0, dp), rec(cost, 1, dp));
-        
-        
+        if(cost.size()==2){
+            return min(cost[0], cost[1]);
+        }
+        vector<int> dp(cost.size(), -1);
+        return min(solve(cost.size()-1, cost, dp), solve(cost.size()-2, cost, dp));
     }
-    int rec(vector<int> &cost, int i, int dp[]){
-        if(i>=cost.size()) return 0;
+    
+    int solve(int i, vector<int> &cost, vector<int> &dp){
+        if(i==0) return cost[i];
+        if(i<0) return 0;
         if(dp[i]!=-1) return dp[i];
-        return dp[i] = cost[i]+min(rec(cost, i+1, dp), rec(cost, i+2, dp));
+        int left = solve(i-1, cost, dp) + cost[i];
+        int right = solve(i-2, cost, dp) + cost[i];
+        return dp[i]=min(left, right);
     }
 };
